5ffe4640d16115527ab1a32c705919eadf79267e
salman6623 <ilhamsalman868@gmail.com>
2020-10-16 00:49:08 +0700

    new

--- src/routers/news/getNews.js
+++ src/routers/news/getNews.js
@@ -1,16 +1,32 @@
 const express = require('express');
 
-const { News } = require('../../models');
+const { News, Users, Category } = require('../../models');
 
 const router = express.Router();
 
-router.get('/news',async(req, res) => {
+const getData = async (id, model) => {
+    const result = await model.findById(id).select('-password'); // 
+    return result;
+}
+
+router.get('/news',async(req, res) => { 
     try {
-        const kumpulan = await News.find({});
-        res.send(kumpulan);
+        const result = await News.find({}); // disini await nya berfungsi untuk disuruh menunggu data seluruh news baru di tampilkan jika seluruh data news sudah muncul
+        const promises = await Promise.all(result.map(async el =>{ // map sama seperrti foreach tetapi bisa memanipulasi data dan 
+            return {
+                judul: el.judul,
+                content: el.content,
+                author: await getData(el.author, Users),
+                category: await getData(el.category, Category)
+            }
+        }));
+        res.send(promises);
+
     }catch(e) {
         res.send({message: e.message});
     }
 });
 
 module.exports = router;
+
+//query dan parameter dan promise
